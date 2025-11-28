866a030f15227bceeb1d70210431d4cf91b5330f
Juan <juandiego.velez@utp.edu.co>
2025-11-28 18:07:05 -0500

    feat: agregar api doc al footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,5 @@
+import { CLOUD_RUN_URL } from "../utils/env";
+
 export default function Footer() {
   const gitLinks = [
     {
@@ -5,7 +7,7 @@ export default function Footer() {
       title: "Iot Frontend",
     },
     {
-      url: "https://github.com/AndresMejia962/iot-backend",
+      url: "https://github.com/jmorales284/IoT-db-distribuida",
       title: "Iot Backend",
     },
   ];
@@ -24,7 +26,7 @@ export default function Footer() {
   return (
     <footer className="bg-white">
       <div className="mx-auto max-w-7xl space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8">
-        <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-4">
           <div>
             <div className="flex items-center gap-3">
               <img src="/iot.png" alt="" />
@@ -37,7 +39,7 @@ export default function Footer() {
             </p>
           </div>
 
-          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:col-span-2">
+          <div className="grid grid-cols-1 gap-8 sm:grid-cols-3 lg:col-span-3">
             <div>
               <h4 className="font-medium text-gray-900">Repositorios</h4>
 
@@ -73,6 +75,22 @@ export default function Footer() {
                     </a>
                   </li>
                 ))}
+              </ul>
+            </div>
+
+            <div>
+              <h4 className="font-medium text-gray-900">Conecta Tu sensor</h4>
+
+              <ul className="mt-6 space-y-4 text-sm">
+                <li>
+                  <a
+                    href={CLOUD_RUN_URL + "/docs"}
+                    target="_blank"
+                    className="text-gray-700 transition hover:opacity-75 flex items-center gap-1"
+                  >
+                    <img src="/api.png" alt="" /> <span>Conecta tu sensor</span>
+                  </a>
+                </li>
               </ul>
             </div>
           </div>
